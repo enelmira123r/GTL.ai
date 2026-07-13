fafcfeea02b97d32063ae68c83654eae1a727c87
Enel <ergimira22@gmail.com>
2026-07-13 20:44:48 +0200

    feat: add 43 Mediaprint digital books to catalog (books.ts)

--- src/books.ts
+++ src/books.ts
@@ -480,5 +480,222 @@ export const BOOKS: CatalogBook[] = [
     "title": "Tik 12 Zgjedhje",
     "klasa": "12",
     "url": "https://botimepegi.al/digital-books/class-12/tik-12-zgjedhje/libri/index.html"
+  },
+
+  // --- Libra digjitalë Mediaprint (mediaprint.al/librat-digjital-2/) ---
+  {
+    "title": "Biologjia 10 11 Pjesa E Dytë",
+    "klasa": "10",
+    "url": "https://mediaprint.al/wp-content/uploads/libra-digital/Biologjia%2010-11%20-%20pjesa%20e%20dyt%C3%AB/index.html"
+  },
+  {
+    "title": "Biologjia 10 11 Pjesa E Parë",
+    "klasa": "10",
+    "url": "https://mediaprint.al/wp-content/uploads/libra-digital/Biologjia%2010-11%20-%20pjesa%20e%20par%C3%AB/index.html"
+  },
+  {
+    "title": "Fizika 10 11 Pjesa E Dytë",
+    "klasa": "10",
+    "url": "https://mediaprint.al/wp-content/uploads/libra-digital/Fizika%2010-11%20-%20pjesa%20e%20dyt%C3%AB/index.html"
+  },
+  {
+    "title": "Fizika 10 11 Pjesa E Parë",
+    "klasa": "10",
+    "url": "https://mediaprint.al/wp-content/uploads/libra-digital/Fizika%2010-11%20-%20pjesa%20e%20par%C3%AB/index.html"
+  },
+  {
+    "title": "Fletore Pune Kimia 10 11",
+    "klasa": "10",
+    "url": "https://mediaprint.al/wp-content/uploads/libra-digital/Fletore%20Pune%20%20Kimia%2010-11/index.html"
+  },
+  {
+    "title": "Fletore Pune Biologjia 10",
+    "klasa": "10",
+    "url": "https://mediaprint.al/wp-content/uploads/libra-digital/Fletore%20Pune%20Biologjia%2010/index.html"
+  },
+  {
+    "title": "Fletore Pune Fizika 10 11",
+    "klasa": "10",
+    "url": "https://mediaprint.al/wp-content/uploads/libra-digital/Fletore%20Pune%20Fizika%2010-11/index.html"
+  },
+  {
+    "title": "Fletore Pune Matematika 1",
+    "klasa": "1",
+    "url": "https://mediaprint.al/wp-content/uploads/libra-digital/Fletore%20Pune%20Matematika%201/index.html"
+  },
+  {
+    "title": "Fletore Pune Matematika 10 11",
+    "klasa": "10",
+    "url": "https://mediaprint.al/wp-content/uploads/libra-digital/Fletore%20Pune%20Matematika%2010%20-11/index.html"
+  },
+  {
+    "title": "Kimia 10 Pjesa E Dyte",
+    "klasa": "10",
+    "url": "https://mediaprint.al/wp-content/uploads/libra-digital/Kimia%2010%20-%20pjesa%20e%20dyte/index.html"
+  },
+  {
+    "title": "Kimia 10 Pjesa E Pare",
+    "klasa": "10",
+    "url": "https://mediaprint.al/wp-content/uploads/libra-digital/Kimia%2010%20-%20pjesa%20e%20pare/index.html"
+  },
+  {
+    "title": "Matematika 1",
+    "klasa": "1",
+    "url": "https://mediaprint.al/wp-content/uploads/libra-digital/Matematika%201/index.html"
+  },
+  {
+    "title": "Matematika 10 Pjesa E Dyte",
+    "klasa": "10",
+    "url": "https://mediaprint.al/wp-content/uploads/libra-digital/Matematika%2010%20-%20pjesa%20e%20%20dyte/index.html"
+  },
+  {
+    "title": "Matematika 10 Pjesa E Pare",
+    "klasa": "10",
+    "url": "https://mediaprint.al/wp-content/uploads/libra-digital/Matematika%2010%20-%20pjesa%20e%20pare/index.html"
+  },
+  {
+    "title": "Matematika 12",
+    "klasa": "12",
+    "url": "https://mediaprint.al/wp-content/uploads/libra-digital/Matematika%2012/index.html"
+  },
+  {
+    "title": "Matematika 5",
+    "klasa": "5",
+    "url": "https://mediaprint.al/wp-content/uploads/libra-digital/Matematika%205/index.html"
+  },
+  {
+    "title": "Matematika 6",
+    "klasa": "6",
+    "url": "https://mediaprint.al/wp-content/uploads/libra-digital/Matematika%206%20/index.html"
+  },
+  {
+    "title": "Matematika 6 Fletore Pune",
+    "klasa": "6",
+    "url": "https://mediaprint.al/wp-content/uploads/libra-digital/Matematika%206%20%20Fletore%20Pune/index.html"
+  },
+  {
+    "title": "Matematika 8",
+    "klasa": "8",
+    "url": "https://mediaprint.al/wp-content/uploads/libra-digital/Matematika%208/index.html"
+  },
+  {
+    "title": "Matematika 9",
+    "klasa": "9",
+    "url": "https://mediaprint.al/wp-content/uploads/libra-digital/Matematika%209/index.html"
+  },
+  {
+    "title": "Matematika Avance 12",
+    "klasa": "12",
+    "url": "https://mediaprint.al/wp-content/uploads/libra-digital/Matematika%20Avance%2012/index.html"
+  },
+  {
+    "title": "Muzika 6",
+    "klasa": "6",
+    "url": "https://mediaprint.al/wp-content/uploads/libra-digital/Muzika%206/index.html"
+  },
+  {
+    "title": "Artipamor 9",
+    "klasa": "9",
+    "url": "https://mediaprint.al/wp-content/uploads/libra-digital/artipamor%209/index.html"
+  },
+  {
+    "title": "Biologji 7",
+    "klasa": "7",
+    "url": "https://mediaprint.al/wp-content/uploads/libra-digital/biologji%207/index.html"
+  },
+  {
+    "title": "Biologjia 12",
+    "klasa": "12",
+    "url": "https://mediaprint.al/wp-content/uploads/libra-digital/biologjia%2012/index.html"
+  },
+  {
+    "title": "Biologjia 9",
+    "klasa": "9",
+    "url": "https://mediaprint.al/wp-content/uploads/libra-digital/biologjia%209/index.html"
+  },
+  {
+    "title": "Dituri Natyre 2",
+    "klasa": "2",
+    "url": "https://mediaprint.al/wp-content/uploads/libra-digital/dituri%20natyre%202/index.html"
+  },
+  {
+    "title": "Dituri Natyre 5",
+    "klasa": "5",
+    "url": "https://mediaprint.al/wp-content/uploads/libra-digital/dituri%20natyre%205/index.html"
+  },
+  {
+    "title": "Edukim Për Shoqërinë 2",
+    "klasa": "2",
+    "url": "https://mediaprint.al/wp-content/uploads/libra-digital/edukim%20p%C3%ABr%20shoq%C3%ABrin%C3%AB%202/index.html"
+  },
+  {
+    "title": "Ekonomia 12",
+    "klasa": "12",
+    "url": "https://mediaprint.al/wp-content/uploads/libra-digital/ekonomia%2012/index.html"
+  },
+  {
+    "title": "Filozofia 11",
+    "klasa": "11",
+    "url": "https://mediaprint.al/wp-content/uploads/libra-digital/filozofia%2011/index.html"
+  },
+  {
+    "title": "Fizika 12",
+    "klasa": "12",
+    "url": "https://mediaprint.al/wp-content/uploads/libra-digital/fizika%2012/index.html"
+  },
+  {
+    "title": "Gjuhë Shqipe 11",
+    "klasa": "11",
+    "url": "https://mediaprint.al/wp-content/uploads/libra-digital/gjuh%C3%AB%20shqipe%2011/index.html"
+  },
+  {
+    "title": "Kimia 12",
+    "klasa": "12",
+    "url": "https://mediaprint.al/wp-content/uploads/libra-digital/kimia%2012/index.html"
+  },
+  {
+    "title": "Kimia 8",
+    "klasa": "8",
+    "url": "https://mediaprint.al/wp-content/uploads/libra-digital/kimia%208/index.html"
+  },
+  {
+    "title": "Kimia 9",
+    "klasa": "9",
+    "url": "https://mediaprint.al/wp-content/uploads/libra-digital/kimia%209/index.html"
+  },
+  {
+    "title": "Muzika 7",
+    "klasa": "7",
+    "url": "https://mediaprint.al/wp-content/uploads/libra-digital/muzika%207/index.html"
+  },
+  {
+    "title": "Psikologjia 12",
+    "klasa": "12",
+    "url": "https://mediaprint.al/wp-content/uploads/libra-digital/psikologjia%2012/index.html"
+  },
+  {
+    "title": "Qytetari 5",
+    "klasa": "5",
+    "url": "https://mediaprint.al/wp-content/uploads/libra-digital/qytetari%205/index.html"
+  },
+  {
+    "title": "Tik 6",
+    "klasa": "6",
+    "url": "https://mediaprint.al/wp-content/uploads/libra-digital/tik%206/index.html"
+  },
+  {
+    "title": "Tik 7",
+    "klasa": "7",
+    "url": "https://mediaprint.al/wp-content/uploads/libra-digital/tik%207/index.html"
+  },
+  {
+    "title": "Tik 8",
+    "klasa": "8",
+    "url": "https://mediaprint.al/wp-content/uploads/libra-digital/tik%208/index.html"
+  },
+  {
+    "title": "Tik 9",
+    "klasa": "9",
+    "url": "https://mediaprint.al/wp-content/uploads/libra-digital/tik%209/index.html"
   }
 ];
